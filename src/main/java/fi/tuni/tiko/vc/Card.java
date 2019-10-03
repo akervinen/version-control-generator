c34334a60cfa37790350d5e6a9d462141cb7ef9e
Jaakko <j.i.mantyla@gmail.com>
2019-10-03 17:28:19 +0300

    Adds constructor, getters and setters for Card.

--- src/main/java/fi/tuni/tiko/vc/Card.java
+++ src/main/java/fi/tuni/tiko/vc/Card.java
@@ -1,5 +1,33 @@
 public class Card{
     private int suit;
     private int value;
+
+    public Card(int value, int suit){
+        setSuit(suit);
+        setValue(value);
+    }
+
+
+    public int getSuit() {
+        return this.suit;
+    }
+
+    public void setSuit(int suit) {
+        if (suit < 0 || suit>3){
+            throw new IllegalArgumentException("Card suit must be 0, 1, 2, or 3");
+        }
+        this.suit = suit;
+    }
+
+    public int getValue() {
+        return this.value;
+    }
+
+    public void setValue(int value) {
+        if (value < 1 || value>13){
+            throw new IllegalArgumentException("Card value must be between 1-13");
+        }
+    }
+
     
 }
