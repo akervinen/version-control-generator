82be954529478fe7ba542f730ee72f5b0033076c
Heikki Kangas <heikki.kangas@tuni.fi>
2019-10-07 01:03:33 +0300

    Update (App): run ManualTestDeck.

--- src/main/java/fi/tuni/tiko/vc/App.java
+++ src/main/java/fi/tuni/tiko/vc/App.java
@@ -2,8 +2,7 @@ package fi.tuni.tiko.vc;
 
 public class App
 {
-    public static void main(String[] args)
-    {
-        System.out.println("Hello World!");
+    public static void main(String[] args) {
+        new ManualTestDeck();
     }
 }
