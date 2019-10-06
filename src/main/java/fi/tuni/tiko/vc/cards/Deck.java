5b779e921ce473548c6189aa30cc067df1f16a36
Heikki Kangas <heikki.kangas@tuni.fi>
2019-10-07 00:20:15 +0300

    Add (cards.Deck): constructor, private initFullDeck(), private getFullDeck().

--- src/main/java/fi/tuni/tiko/vc/cards/Deck.java
+++ src/main/java/fi/tuni/tiko/vc/cards/Deck.java
@@ -8,4 +8,25 @@ import java.util.stream.Collectors;
 public class Deck {
     private LinkedList<Card> cards;
     private LinkedList<Card> fullDeck;
+
+    public Deck() {
+        initFullDeck();
+        cards = getFullDeck();
+    }
+
+    private void initFullDeck() {
+        fullDeck = new LinkedList<>();
+        for (int value = 2; value < 15; value++) {
+            for (int suit = 0; suit < 4; suit++) {
+                fullDeck.add(new Card(value, suit));
+            }
+        }
+        cards = getFullDeck();
+    }
+
+    private LinkedList<Card> getFullDeck() {
+        return fullDeck
+                .stream()
+                .collect(Collectors.toCollection(LinkedList::new));
+    }
 }
