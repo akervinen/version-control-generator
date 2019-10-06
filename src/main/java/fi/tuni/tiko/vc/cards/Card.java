ad36c015f00dcdf577dba8af58dd729987ad0c87
Heikki Kangas <heikki.kangas@tuni.fi>
2019-10-06 22:08:41 +0300

    Add (cards.Card): getters and setters for value and suit.

--- src/main/java/fi/tuni/tiko/vc/cards/Card.java
+++ src/main/java/fi/tuni/tiko/vc/cards/Card.java
@@ -6,4 +6,20 @@ package fi.tuni.tiko.vc.cards;
 public class Card {
     private int value;
     private int suit;
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public int getSuit() {
+        return suit;
+    }
+
+    public void setSuit(int suit) {
+        this.suit = suit;
+    }
 }
