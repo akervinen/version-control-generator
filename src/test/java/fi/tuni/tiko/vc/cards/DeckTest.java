6a85bc8d53c617fade32351c4a8934dd84bccf38
Heikki Kangas <heikki.kangas@tuni.fi>
2019-10-07 17:20:08 +0300

    Add (DeckTest): testResetDeck.

--- src/test/java/fi/tuni/tiko/vc/cards/DeckTest.java
+++ src/test/java/fi/tuni/tiko/vc/cards/DeckTest.java
@@ -9,4 +9,14 @@ public class DeckTest {
     public DeckTest() {
         deck = new Deck();
     }
+
+    @Test
+    public void testResetDeck() {
+        String originalDeck = deck.toString();
+        deck.resetDeck();
+        String shuffledDeck = deck.toString();
+
+        // Tests if Deck's output changes when the deck is reset.
+        Assert.assertFalse(originalDeck.equals(shuffledDeck));
+    }
 }
